9487182b27c3f3eca51f5739da628ebab449fcd8
Mauricio Barrios <pedidoshmb@gmail.com>
2025-01-14 16:59:08 -0500

    actualiza main js

--- 09-bucles/main.js
+++ 09-bucles/main.js
@@ -1,5 +1,17 @@
-//Bucle for
+/*
 
-//Es uan estructura de control que ejecuta un bloque de codigo varias veces de forma automatica
-//mientras se cumpla una condicion.
+for(inicializacion; condicion; incremento){
 
+    //codigo que se ejecuta mientras la condicion se cumple
+}
+
+*/
+
+// Ejemplo:
+
+let limite = 177;
+
+for(let contador = 0; contador <=limite; contador++){
+    //Bloque de codigo que se va a ejecutar
+    console.log("Vamor por el numero :"+contador);
+}

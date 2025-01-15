367c3a69bfad23a6fefee12e155d90a28b0b6176
Mauricio Barrios <pedidoshmb@gmail.com>
2025-01-15 15:44:43 -0500

    Ejercicio 19 FOR numeros divisores de un numero que digite el usuario

---
+++ js/12-ejercicios/ejercicio19.js
@@ -0,0 +1,19 @@
+/*
+Ejercicio 19 
+
+Muestra todos los numeros divisores de un numero que se mete en el prompt
+
+
+for(let i = 1; i <=10; i++)
+
+*/
+
+let numero = parseInt(prompt("Digite el numero :", 1 ));
+
+for(let i = 1; i<=numero; i++ ){
+
+    if(numero % i == 0){
+        console.log(`El ${i} es divisor de: ${numero}`);
+    }
+}
+    //console.log(`El ${contador} es divisor de: ${numero}`);

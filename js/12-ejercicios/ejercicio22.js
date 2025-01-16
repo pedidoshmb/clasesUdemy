08d13ebff170aa8092cad3fb3f302b9b2a741796
Mauricio Barrios <pedidoshmb@gmail.com>
2025-01-16 10:28:35 -0500

    ejercicio con while restando 1 unidad a una variable en cada Bucle

---
+++ js/12-ejercicios/ejercicio22.js
@@ -0,0 +1,21 @@
+/*
+Ejercicio 22:
+
+Tenemos una bolsa con 37 caramelos, cada vez que te comes uno quedan menos.
+
+haz una simulacion con un bucle
+
+for(let i = 1; i<=numero; i++ )
+
+*/
+
+let bolsa = 37
+
+while(bolsa >0 ){
+    //Mostrar algo
+    console.log("Te acabas de comer un caramelo. Quedan"+bolsa+" Caramelos en la bolsa");
+
+     //Bolsa = bolsa -1
+    bolsa--;
+}
+   
